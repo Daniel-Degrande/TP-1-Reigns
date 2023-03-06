6615b3eeb3e512f8a3caaa45f64f2b625b2b67eb
danie <danie@LAPTOP-Q0KIHKHF>
2023-03-06 16:09:15 +0100

    Ajout classe "InterfaceTexte" -> Sera utilisée pour gérer l'entrée du texte saisi au clavier (Et aussi pour retirer la gestion de la saisie clavier de la classe Reigns)

---
+++ src/main/InterfaceTexte.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Scanner;
+
+public class InterfaceTexte {
+
+
+    private Scanner fluxIn;
+    InterfaceTexte(){
+        fluxIn = new Scanner(System.in);
+    }
+
+    public String entrerTexteLibre(String Question){
+        return "";
+    }
+}
