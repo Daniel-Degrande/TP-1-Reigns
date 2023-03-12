733a8a42d1219eecdae9bcbb464c750cfb61100f
danie <danie@LAPTOP-Q0KIHKHF>
2023-03-12 18:13:42 +0100

    Déplacement méthodes "initBanqueQuestion" et "getQuestionAleatoire" dans BanqueQuestion.java

--- src/main/Reigns.java
+++ src/main/Reigns.java
@@ -22,7 +22,7 @@ public class Reigns {
     /**
      * la banque de questions
      */
-    private static ArrayList<Question> questions;
+    private static BanqueQuestion questions;
 
     /**
      * La méthode main lance le jeu Reigns. Il initialise les questions, le personnage,
@@ -47,7 +47,7 @@ public class Reigns {
     private static void initialiser_partie(){
         System.out.println("Bienvenue sur Reigns");
 
-        initBanqueQuestions();
+        questions.initListe();
 
         System.out.println("Création du personnage...");
 
@@ -63,7 +63,7 @@ public class Reigns {
         int nbTours = 0;
         while(!personnage.finDuJeu()){
             nbTours++;
-            Question question = getQuestionAleatoire();
+            Question question = questions.getQuestionAleatoire();
             reponseQuestion(question);
             personnage.AfficheJauges();
         }
@@ -104,75 +104,20 @@ public class Reigns {
         }
     }
 
-    /**
+    /*
      * Cette fonction permet d'initialiser le personnage joué. Elle demande à l'utilisateur de saisir le nom du personnage
      * et le genre (Roi ou Reine). Elle crée ensuite le personnage.
      */
 
-    private static void initPersonnage(){
 
-    }
-
-    /**
+    /*
      * Cette fonction initialise la banque de questions. Elle crée les questions et les ajoute à la banque.
      */
-    private static void initBanqueQuestions(){
-        questions = new ArrayList<>();
-        Question question1 = new Question(
-                "Main du roi",
-                "Le peuple souhaite libérer les prisonniers",
-                "Oui",
-                "Non");
-        question1.ajouteEffetGauche(TypeJauge.ARMEE, -5);
-        question1.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
-        question1.ajouteEffetDroite(TypeJauge.PEUPLE, -7);
-        questions.add(question1);
-        Question question2 = new Question(
-                "Paysan",
-                "Il n'y a plus rien à manger",
-                "Importer de la nourriture",
-                "Ne rien faire");
-        question2.ajouteEffetGauche(TypeJauge.FINANCE,-5);
-        question2.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
-        question2.ajouteEffetDroite(TypeJauge.PEUPLE, -5);
-        questions.add(question2);
-        Question question3 = new Question(
-                "Prêtre",
-                "Les dieux sont en colère",
-                "Faire un sacrifice",
-                "Ne rien faire");
-        question3.ajouteEffetGauche(TypeJauge.CLERGE, +5);
-        question3.ajouteEffetGauche(TypeJauge.PEUPLE, -3);
-        question3.ajouteEffetDroite(TypeJauge.CLERGE, -5);
-        questions.add(question3);
-        Question question4 = new Question(
-                "Main du roi",
-                "Le roi Baratheon rassemble son armée",
-                "Le soutenir",
-                "Rester neutre");
-        question4.ajouteEffetGauche(TypeJauge.ARMEE, +3);
-        question4.ajouteEffetGauche(TypeJauge.FINANCE, -3);
-        question4.ajouteEffetGauche(TypeJauge.CLERGE, -3);
-        question4.ajouteEffetDroite(TypeJauge.PEUPLE, +3);
-        questions.add(question4);
-        Question question5 = new Question(
-                "Paysan",
-                "Abondance de récoltes cette année",
-                "Taxer énormément",
-                "Taxer un tout petit peu");
-        question5.ajouteEffetGauche(TypeJauge.FINANCE, +10);
-        question5.ajouteEffetGauche(TypeJauge.PEUPLE, -5);
-        question5.ajouteEffetDroite(TypeJauge.FINANCE, +1);
-        question5.ajouteEffetDroite(TypeJauge.PEUPLE, -3);
-        questions.add(question5);
-    }
 
-    /**
+
+    /*
      * Cette fonction permet de tirer une question aléatoire dans la banque de questions.
      * @return Une question aléatoire
      */
-    private static Question getQuestionAleatoire(){
-        int numQuestion = (int) (Math.random()*questions.size());
-        return questions.get(numQuestion);
-    }
+
 }
